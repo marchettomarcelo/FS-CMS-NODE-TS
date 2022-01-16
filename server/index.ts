0c2ca4b0d182b32a0780f586921be0ec8f0a1538
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-16 10:50:37 -0300

    teste4

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import Post from "./models/post"
 require("mongoose");
 
 //TYPESCRIPT TEST FILE
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../my-app/build")));
