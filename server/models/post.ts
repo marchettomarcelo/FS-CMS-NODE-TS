12a38d65846ebf5d69db37fd81cf5fe32bc514ff
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-19 11:42:25 -0300

    prod test

--- server/models/post.ts
+++ server/models/post.ts
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://127.0.0.1:27017/cms";
-mongoose.connect(url, { useNewUrlParser: true });
+// const url = "mongodb://127.0.0.1:27017/cms";
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 
 const Post = mongoose.model("Posts", {
   titulo: { type: String, required: true, trim: true },
