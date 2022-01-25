28ce8dca0ea98eed691f9891aac161170b06058d
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-25 10:39:18 -0300

    changes in canva

--- my-app/src/components/Canva.tsx
+++ my-app/src/components/Canva.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Post, CanvaProps } from "../react-app-env";
+import SwitchItem from "./SwitchItem"
 
 
 export default function Canva({editingNow, postFoiEditado, deleteEditingNow }: CanvaProps) {
@@ -17,8 +18,7 @@ export default function Canva({editingNow, postFoiEditado, deleteEditingNow }: C
   }, [editingNow]);
 
   //notifiing parent component of changes on the current post
-  const HandleChange = async (e:any) => {
-
+  const HandleTypeChanges = async (e:any) => {
     let novo = {
       ...editingNowInCanva,
       [e.target.id]: e.target.value,
@@ -27,41 +27,74 @@ export default function Canva({editingNow, postFoiEditado, deleteEditingNow }: C
     setEditingNowInCanva(novo);
   };
 
+  // const HandleSwitchChanges(SwitchValue:boolean){
+  //   let novo = {
+  //     ...editingNowInCanva,
+  //     Publish: SwitchValue 
+  //   };
+  //   postFoiEditado(novo);
+  //   setEditingNowInCanva(novo);
+
+  // }
+
   return (
     <div
       className="flex flex-col border-4 border-black border-solid 
-    w-9/12  h-11/12 rounded shadow-2xl p-4 gap-4 "
+    w-9/12  h-11/12 rounded shadow-2xl gap-4"
     >
-      <textarea
-        className="text-5xl font-extrabold  p-2
-        border border-solid border-gray-300 rounded
-        focus:bg-white focus:border-gray-600 focus:outline-none
-        transition
-        ease-in-out"
-        name="titulo"
-        id="titulo"
-        rows={2}
-        value={editingNowInCanva.titulo}
-        onChange={HandleChange}
-      />
-      <textarea
-        className="text-2xl font-extrabold  p-2
-        border border-solid border-gray-300 rounded
-        focus:bg-white focus:border-gray-600 focus:outline-none
-        transition
-        ease-in-out"
-        name="conteudo"
-        id="conteudo"
-        rows={5}
-        onChange={HandleChange}
-        value={editingNowInCanva.conteudo}
-      />
+      <div className=" overflow-y-auto  m-4 overscroll-non">
+        <textarea
+          className="text-5xl font-extrabold  p-2 w-full
+          border border-solid border-gray-300 rounded
+          focus:bg-white focus:border-gray-600 focus:outline-none
+          transition
+          ease-in-out"
+          name="titulo"
+          id="titulo"
+          rows={2}
+          value={editingNowInCanva.titulo}
+          onChange={HandleTypeChanges}
+        />
+        <textarea
+          className="text-2xl font-extrabold  p-2 w-full
+          border border-solid border-gray-300 rounded
+          focus:bg-white focus:border-gray-600 focus:outline-none
+          transition
+          ease-in-out"
+          name="conteudo"
+          id="conteudo"
+          rows={5}
+          onChange={HandleTypeChanges}
+          value={editingNowInCanva.conteudo}
+        />
 
-      <img src={require("../assets/X-cross.png")} 
-      alt="Close icon" 
-      className="bottom-8 right-8 absolute w-12 cursor-pointer"
-      onClick={deleteEditingNow}
-      />
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+      </div>
+      
+      <div className=" bg-gray-300 border-t-4 mx-0 border-solid border-black h-20 w-full items-center p-4
+      flex bottom-1 mt-auto">
+      
+      
+        <h1 className="mr-2">Desabilitar postagem</h1>
+        <SwitchItem />
+
+
+  
+
+        <img src={require("../assets/X-cross.png")} 
+        alt="Close icon" 
+        className="w-14 h-14 cursor-pointer ml-auto"
+        onClick={deleteEditingNow}
+        />
+
+      </div>
+      
     </div>
   );
 }
