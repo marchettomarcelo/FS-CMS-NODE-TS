360fa80a86fe9637cb70af5ccd369035ee4c248b
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-28 17:46:51 -0300

    minor change

--- my-app/src/components/Canva.tsx
+++ my-app/src/components/Canva.tsx
@@ -84,15 +84,7 @@ export default function Canva({editingNow, postFoiEditado, deleteEditingNow }: C
 
 
   
-        {/* <svg xmlns="http://www.w3.org/2000/svg"
-         className="w-14 h-14 cursor-pointer ml-auto" 
-         fill="none" 
-         viewBox="0 0 24 24" 
-         stroke="#ff000d"
-         onClick={deleteEditingNow}
-         >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg> */}
+      
 
         <svg xmlns="http://www.w3.org/2000/svg" 
         className="w-12 h-12 cursor-pointer ml-auto" 
